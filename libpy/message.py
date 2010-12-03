1e4f73dd4802317f70f65afe1b7264e47eed1fae
Lars Orum Rasmussen <ras@dmi.dk>
2010-12-03 13:06:30 +0000

    more flexible decoding

--- libpy/message.py
+++ libpy/message.py
@@ -1,9 +1,11 @@
 # -*-python-*- 
 #
-"""A Message, goes like: 
+"""A Message goes like: 
 <subject> <type> <timestamp> <sender> [data]
 
-fx: pytroll:/DC/address info 2010-12-01T12:21:11 juhu@prodsat prodsat:22010
+Message('pytroll:/DC/juhu', 'info', 'jhuuuu !!!')
+will be encoded as (at the right time and by the right user at the right host):
+pytroll:/DC/juhu info 2010-12-01T12:21:11 henry@prodsat jhuuuu !!!
 """
 import re
 from datetime import datetime
@@ -11,6 +13,11 @@ from datetime import datetime
 class MessageError(Exception):
     pass
 
+#-----------------------------------------------------------------------------
+#
+# Utillities.
+#
+#-----------------------------------------------------------------------------
 def is_valid_subject(s):
     """Currently we only check for empty stings
     """
@@ -26,32 +33,23 @@ def is_valid_sender(s):
     """
     return isinstance(s, str) and bool(s)
 
-def _strptime(str):
-    _isoformat = "%Y-%m-%dT%H:%M:%S.%f"
-    return datetime.strptime(str, _isoformat)
-
-def _getsender():
-    import getpass
-    import socket
-    host = socket.gethostname()
-    user = getpass.getuser()
-    return "%s@%s"%(user, host)
-
+#-----------------------------------------------------------------------------
+#
+# Message class.
+#
+#-----------------------------------------------------------------------------
 class Message:
 
   """A Message.
 
   - Has to be initialized with a 'subject', 'type' and optionally 'data'.
-  - It will add a few extra attributes.
+  - It will add add few extra attributes.
   - It will make a Message pickleable."""
 
   def __init__(self, subject='', atype='', data='', empty=False):
       """A Message needs at least a subject and a type ... if not specified as empty.
       """
-      if empty:
-          self.subject, self.type, self.time, self.sender, self.data = \
-              ('', '', '', '', '')
-      else:
+      if not empty:
           self.subject = subject
           self.type = atype
           self.time = datetime.utcnow()
@@ -76,24 +74,14 @@ class Message:
   @staticmethod
   def decode(rawstr):
       m = Message(empty=True)
-      a = re.split(r"\s+", rawstr, maxsplit=4)
-      if len(a) < 4:
-          raise MessageError, "Could node decode raw string: '%s ...'"%str(rawstr[:36])
-      m.subject = a[0].strip()
-      m.type = a[1].strip()
-      m.time = _strptime(a[2].strip())
-      m.sender = a[3].strip()
-      try:
-          m.data = a[4]
-      except IndexError:
-          m.data = ''
+      m.__dict__ = _decode(rawstr)
       m._validate()
       return m
 
   def encode(self):
       self._validate()
-      text = "%s %s %s %s"%(self.subject, self.type, self.time.isoformat(), self.sender)
-      return text + ' ' + self.data
+      rawstr = "%s %s %s %s"%(self.subject, self.type, self.time.isoformat(), self.sender)
+      return rawstr + ' ' + self.data
 
   def __repr__(self):
       return self.encode()
@@ -117,20 +105,51 @@ class Message:
 
   def __setstate__(self, state):
       self.__dict__.clear()
-      self.__dict__ = Message.decode(state).__dict__
+      self.__dict__ = _decode(state)
+
+
+#-----------------------------------------------------------------------------
+#
+# Small internal helpers.
+#
+#-----------------------------------------------------------------------------
+def _decode(rawstr):
+    a = re.split(r"\s+", rawstr, maxsplit=4)
+    if len(a) < 4:
+        raise MessageError, "Could node decode raw string: '%s ...'"%str(rawstr[:36])
+    d = dict((('subject', a[0].strip()),
+              ('type', a[1].strip()),
+              ('time', _strptime(a[2].strip())),
+              ('sender', a[3].strip())))
+    try:
+        d['data'] = a[4]
+    except IndexError:
+        d['data'] = ''
+    return d
+
+def _strptime(str):
+    _isoformat = "%Y-%m-%dT%H:%M:%S.%f"
+    return datetime.strptime(str, _isoformat)
+
+def _getsender():
+    import getpass
+    import socket
+    host = socket.gethostname()
+    user = getpass.getuser()
+    return "%s@%s"%(user, host)
 
 if __name__ == '__main__':
     import pickle
     import os
 
     m1 = Message('/test/whatup/doc', 'info', data='not much to say')
-    uh = '%s@%s'%(m1.user, m1.host)
-    print uh
-    if uh != m1.sender:
+    sender = '%s@%s'%(m1.user, m1.host)
+    print sender
+    if sender != m1.sender:
         print 'OOPS ... deconding sender failed'
     m2 = Message.decode(m1.encode())
     print m2
-    if uh != m1.sender:
+    if str(m2) != str(m1):
         print 'OOPS ... decoding/encoding message failed'  
     rawstr = "/test/what/todo info 2008-04-11T22:13:22.123000 ras@hawaii what's up doc"
     m = Message.decode(rawstr)
