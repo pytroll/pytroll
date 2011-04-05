d58c8d0b1b000340ba08fef71b5157b982393160
Esben S. Nielsen <esn@dmi.dk>
2011-04-05 13:19:29 +0200

    added sqlalchemy geography type

--- db/pytroll_db.py
+++ db/pytroll_db.py
@@ -11,6 +11,7 @@ from sqlalchemy.databases.postgres import PGBinary
 import shapely.wkb
 import shapely.wkt
 
+from sqltypes import LINESTRING
 
 Base = declarative_base()
 
@@ -34,10 +35,39 @@ class ParameterType(Base):
     __tablename__ = 'parameter_type'
 
     #mapping
-    parameter_type_id = 
-    parameter_type_name = 
-    parameter_location = 
+    parameter_type_id = Column(Integer, primary_key=True)
+    parameter_type_name = Column(String)
+    parameter_location = Column(String)
     
+    def __init__(self, parameter_type_id, parameter_type_name, parameter_location):
+        self.parameter_type_id = parameter_type_id
+        self.parameter_type_name = parameter_type_name
+        self.parameter_location = parameter_location
+
+
+class Parameter(Base):
+    __tablename__ = 'parameter'
+
+    #mapping
+    parameter_id = Column(Integer, primary_key=True)
+    parameter_type_id = Column(Integer)
+    parameter_name = Column(String)
+    description = Column(String)
+    
+    def __init__(self, parameter_id, parameter_type_id, parameter_name, description):
+        self.parameter_id = parameter_id
+        self.parameter_type_id = parameter_type_id
+        self.parameter_name = parameter_name
+        self.description = description
+
+"""
+class Tag(Base):
+    __tablename__ = 'tag'
+
+    #mapping
+    tag_id = Column(Integer, primary_key=True)
+    tag = Column(String)
+
     def __init__(self,):
 
 
@@ -48,6 +78,12 @@ class (Base):
     
     def __init__(self,):
 
+class (Base):
+    __tablename__ = ''
+
+    #mapping
+    
+    def __init__(self,):
 
 class (Base):
     __tablename__ = ''
@@ -56,13 +92,13 @@ class (Base):
     
     def __init__(self,):
 
-
 class (Base):
     __tablename__ = ''
 
     #mapping
     
     def __init__(self,):
+"""
 
 
 class ParameterLinestring(Base):
@@ -72,7 +108,8 @@ class ParameterLinestring(Base):
     filename = Column(String, primary_key=True)
     parameter_id = Column(Integer, primary_key=True)
     creation_time = Column(DateTime)
-    data_value = Column(PGBinary)
+    #data_value = Column(PGBinary)
+    data_value = Column(LINESTRING())
 
     def __init__(self, filename, parameter_id, creation_time, data_value):
         self.filename = filename
@@ -102,11 +139,11 @@ class ReportManager(object):
         wkt_o = shapely.wkt.loads(line_s)
         print wkt_o
         wkb_o = wkt_o.wkb #shapely.wkb.dumps(wkt_o)
-        print wkb_o
+        print type(wkb_o)
 
         print type(wkb_o)
-        param_track = ParameterLinestring("hrpt_201012011615_lvl0_smb.l0", 8, datetime.datetime.utcnow(), wkb_o.encode('hex'))
-        
+        #param_track = ParameterLinestring("hrpt_201012011615_lvl0_smb.l0", 8, datetime.datetime.utcnow(), wkb_o.encode('hex'))
+        param_track = ParameterLinestring("hrpt_201012011615_lvl0_smb.l0", 8, datetime.datetime.utcnow(), wkt_o)
         self._session.add(param_track)
         self._session.commit()
 
@@ -116,12 +153,13 @@ if __name__ == '__main__':
     res = pt.first()
     print type(res.data_value)
     #t_geo = ogr.CreateGeometryFromWkb(res.track.decode('hex'))
-    t_geo = shapely.wkb.loads(res.data_value.decode('hex')).wkt
-    
+    #t_geo = shapely.wkb.loads(res.data_value.decode('hex')).wkt
+    t_geo = res.data_value.wkt    
+
     print res.filename, res.parameter_id, res.creation_time, t_geo
     
     # insert into database
-    #rm.add_param_track()
+    rm.add_param_track()
     #pt = rm.get_param_track()
     #res = pt.first()
     
