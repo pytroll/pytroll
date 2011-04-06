809014de892abc5c0961c51c7211f1a26b403ffc
Esben S. Nielsen <esn@dmi.dk>
2011-04-06 12:05:32 +0200

    ...

--- db/sqltypes.py
+++ db/sqltypes.py
@@ -2,18 +2,8 @@ import sqlalchemy
 
 from shapely import geometry, wkb
 
-
-def bind_processor(self, dialect):
-    """Convert from Python type to database type."""
-    def process(value):
-        """``value`` is a Python/Shapely geometry object."""
-        if value is None:
-            return None
-        else:
-            return 'SRID=%s;%s' % (self.SRID, value)
-    return process
-
-def result_processor(self, dialect):
+'''
+def result_processor(self, dialect, coltype):
     """Convert from database type to Python type."""
     
     def process(value):
@@ -23,6 +13,7 @@ def result_processor(self, dialect):
         else:
             return wkb.loads(value.decode('hex'))
     return process
+
 
 def process_bind_param(self, value, dialect):
     """Convert from Python type to database type."""
@@ -42,6 +33,7 @@ def process_result_value(self, value, dialect):
         return None
     else:
         return wkb.loads(value.decode('hex'))
+'''
 
 class Geography(sqlalchemy.types.TypeEngine):
     """PostGIS Geometry Type."""
@@ -52,18 +44,39 @@ class Geography(sqlalchemy.types.TypeEngine):
         self.type = type_.upper()
         self.dimension = dimension
         
+    def bind_processor(self, dialect):
+        """Convert from Python type to database type."""
+        def process(value):
+            """``value`` is a Python/Shapely geometry object."""
+            if value is None:
+                return None
+            else:
+                return 'SRID=%s;%s' % (self.SRID, value)
+        return process
+
+    def result_processor(self, dialect, *args):
+        """Convert from database type to Python type."""
+        
+        def process(value):
+            """``value`` is a hex-encoded WKB string."""
+            if value is None:
+                return None
+            else:
+                return wkb.loads(value.decode('hex'))
+        return process
     
     def get_col_spec(self):
         return 'GEOGRAPHY'
 
+"""
 sqla_version = sqlalchemy.__version__
 if float(sqla_version.rsplit('.', 1)[0]) < 0.6:
     Geography.bind_processor = bind_processor
     Geography.result_processor = result_processor  
 else:
-    Geography.process_bind_param = process_bind_param
-    Geography.process_result_value = process_result_value 
-
+    Geography.bind_processor = bind_processor
+    Geography.result_processor = result_processor 
+"""
     
 class POINT(Geography):
     def __init__(self):
