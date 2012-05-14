3679880b345900e4451d14b7f291e4b1104c7582
Lars Orum Rasmussen <ras@dmi.dk>
2012-05-14 15:13:53 +0000

    Start on polar_preproc lib

---
+++ polar_preproc/libpy/polar_preproc/npp_granule.py
@@ -0,0 +1,51 @@
+import os
+from datetime import datetime, timedelta
+import glob
+import re
+
+from polar_preproc import _dte2time
+from porlar_proprec.segment import Segment
+
+_RE_GRANULE_FIELDS = re.compile('(?P<kind>[-A-Z]+)(?P<band>[0-9]*)_' +
+                                '(?P<satname>[A-Za-z0-9]+)_'+
+                                'd(?P<date>\d+)_t(?P<start_time>\d+)_' +
+                                'e(?P<end_time>\d+)_b(?P<orbit>\d+)_' +
+                                'c(?P<create_time>\d+)_(?P<site>[a-zA-Z0-9]+)_' +
+                                '(?P<domain>[a-zA-Z0-9]+)\.h5')
+
+class NPPGranule(Segment):
+
+    description = "NPP Granule"
+
+    def __init__(self, satellite_name='npp', start_time=None, **kwargs):
+        super(NPPGranule, self).__init__(satellite_name=satellite_name,
+                                         start_time=start_time,
+                                         **kwargs)
+        self.items.sort()
+        self.check_stamps()
+
+    def check_stamps(self):
+        stamp = self.stamp
+        for fn in self.items:
+            fn = os.path.basename(fn)
+            if get_npp_stamp(fn) != stamp:
+                raise IOError(
+                    "Mismatch in npp stamp for '%s', expected '%s'" % 
+                    (fn, stamp))
+
+def get_granule_fields(filename, decode=True):
+    """Extract the fields of an NPP CDFCB-format filename.
+    PFE: adl_geo_ref.py
+    """
+    nfo = dict(_RE_GRANULE_FIELDS.match(os.path.basename(filename)
+                                        ).groupdict())
+    if decode:
+        if nfo['band']:
+            nfo['band'] = int(nfo['band'])
+        nfo['orbit'] = int(nfo['orbit'])
+        nfo['start_time'], nfo['end_time'] = (
+            _dte2time(nfo['date'], nfo['start_time'], nfo['end_time']))
+        nfo['create_time'] = datetime.strptime(nfo['create_time'],
+                                               '%Y%m%d%H%M%S%f')
+        del nfo['date']
+    return nfo
---
+++ polar_preproc/libpy/polar_preproc/segment.py
@@ -0,0 +1,115 @@
+#
+# A general segment.
+#
+import os
+import re
+from datetime import datetime
+import json
+
+from polar_preproc import NPPStamp
+
+_RE_TIME_ISOFORMATS = [(re.compile(
+            '^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$'),
+                        "%Y-%m-%dT%H:%M:%S"),
+                       (re.compile(
+            '^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+$'),
+                        "%Y-%m-%dT%H:%M:%S.%f")]
+
+class Segment(object):
+    description = None
+    use_stamp_in_list = True
+
+    def __init__(self, satellite_name=None, start_time=None, end_time=None, orbit_number=None,
+                 create_time=None, site=None, domain=None, name=None,
+                 description=None, path='', items=[]):
+        self.satellite_name = satellite_name
+        self.start_time = start_time
+        self.end_time = end_time
+        self.orbit_number = orbit_number
+        self.create_time = create_time or datetime.now()
+        self.site = site or SITE
+        self.domain = domain or DOMAIN
+        self.name = name
+        self.description = description or self.description # convert to an attribute
+        self._attrs = self.__dict__.keys()
+        self.path = path
+        self.items = items
+
+    def append(self, item, start_time=None, end_time=None, orbit_number=None):
+        if item in self.items:
+            return
+        if len(self.items) == 0:
+            self.start_time = start_time or item.start_time
+            self.orbit_number = orbit_number or item.orbit_number
+        self.end_time = end_time or item.end_time         
+        self.items.append(item)
+
+    def extend(self, items, start_time=None, end_time=None, orbit_number=None):
+        for i in items:
+            self.append(i, start_time, end_time, orbit_number)
+
+    @property
+    def stamp(self):
+        self._check()
+        return str(NPPStamp(self.satellite_name, self.start_time, self.end_time,
+                            self.orbit_number))
+
+    def dump(self, filename):
+        self._check()
+        json_dump(self, filename)
+
+    def __str__(self):
+        self._check()
+        return json_dumps(self)
+
+    def is_valid(self):
+        return bool(self.satellite_name and self.start_time and
+                    self.end_time and self.orbit_number)
+
+    def _check(self):
+        if not self.is_valid():
+            raise TypeError("Not a valid segmet, missing attributes.")
+
+def json_dumps(seg):
+    nfo = {}
+    for k in seg._attrs:
+        v = getattr(seg, k)
+        if v != None:
+            if isinstance(v, datetime):
+                nfo[k] = v.isoformat()
+            else:
+                nfo[k] = v
+    items = []
+    for i in seg.items:
+        if seg.use_stamp_in_list:
+            try:
+                i = i.stamp
+            except AttributeError:
+                pass
+        items.append(i)
+    return json.dumps((nfo, seg.path, items),
+                      sort_keys=True, indent=4)
+
+def json_dump(seg, filename):
+    with open(filename, 'w') as fp:
+        fp.write(json_dumps(seg) + '\n')
+     
+def json_loads(blob):
+    nfo, path, items = json.loads(blob)
+    for key, val in nfo.items():
+        for fmt in _RE_TIME_ISOFORMATS:
+            if fmt[0].match(str(val)):
+                nfo[key] = datetime.strptime(val, fmt[1])
+    nfo['path'] = path
+    nfo['items'] = items
+    return Segment(**nfo)
+
+def json_load(filename):
+    with open(filename) as fp:
+        return json_loads(fp.read())
+
+if __name__ == '__main__':
+    import sys
+    s = json_load(sys.argv[1])
+    print s
+    json_dump(s, "xxx.json")
