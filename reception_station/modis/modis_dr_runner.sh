bf90afa4931f6e569e0761efce35a19a007b81df
Adam Dybbroe <adam.dybbroe@smhi.se>
2012-10-02 16:50:39 +0200

    Set the logfile

--- reception_station/modis/modis_dr_runner.sh
+++ reception_station/modis/modis_dr_runner.sh
@@ -24,6 +24,7 @@ utv)
 SPA_HOME="/local_disk/opt/SPA"
 APPL_HOME="${HOME}/usr"
 MODIS_LVL1PROC_CONFIG_DIR="/data/proj/safutv/dev/npp_dev/pytroll/reception_station/modis/etc"
+MODIS_LVL1PROC_LOG_FILE="/data/proj/safutv/dev/npp_dev/pytroll/reception_station/modis/logs/modis_lvl1proc.log"
 
         ;;
 
@@ -66,6 +67,7 @@ esac
 
 export SPA_HOME
 export MODIS_LVL1PROC_CONFIG_DIR
+export MODIS_LVL1PROC_LOG_FILE
 
 MODIS_LVL1PROC=${APPL_HOME}
 export MODIS_LVL1PROC
