600eb7cb300d368e196a8961af69718776d9c4dc
Martin Raspaud <martin.raspaud@smhi.se>
2012-11-07 15:38:26 +0100

    Make scisys receiver installable.

---
+++ reception_station/scisys/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2012 Martin Raspaud
+
+# Author(s):
+
+#   Martin Raspaud <martin.raspaud@smhi.se>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Setup for scisys reception system interface with pytroll.
+"""
+
+
+from setuptools import setup
+
+setup(name="SMHI-posttroll",
+      version="0.2.0",
+      description="Scisys message interface to pytroll",
+      author='The pytroll team',
+      author_email='martin.raspaud@smhi.se',
+      #packages=['scisys_receiver'],
+      scripts = ['bin/scisys_receiver.py'],
+      zip_safe=False,
+      license="GPLv3",
+      install_requires=["posttroll"],
+      extras_require={ 'daemon': ['python-daemon']},
+      )
