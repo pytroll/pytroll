0a93208c1637b35995443943897596c503ea7f5c
Adam Dybbroe <adam.dybbroe@smhi.se>
2012-12-13 14:38:17 +0100

    No title on the side bar only logo

--- web/source/conf.py
+++ web/source/conf.py
@@ -106,7 +106,8 @@ html_theme_options = {'bodyfont': 'Sans-serif',
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Pytroll"
+#html_title = "Pytroll"
+html_title = ""
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
